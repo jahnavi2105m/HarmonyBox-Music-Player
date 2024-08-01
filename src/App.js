cff84ebdd9f61954b540edc5a3ae5bc7316dc8df
Jahnavi Malhotra <jahnavim2105@gmail.com>
2024-08-01 13:44:41 -0400

    images and songs update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import {useState} from 'react';
 
 function App() {
+  const [song, setSong] = useState([
+    {
+      title: "I Was Made For Loving You",
+      artist: "KISS",
+      img_src: "./images/kiwmfly-img.png",
+      src: "./music/Kiss - I Was Made For Lovin You.m4a"
+    },
+    {
+      title: "Dancing Queen",
+      artist: "ABBA",
+      img_src: "./images/dancingqueen-img.png",
+      src: "./music/Abba - Dancing Queen.m4a"
+    }
+  ])
+
+
   return (
     <div className="App">
-      App
+      
     </div>
   );
 }
