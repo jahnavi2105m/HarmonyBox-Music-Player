fe16478ca35b3e54eb3a42fadc10a3301f034b92
Jahnavi Malhotra <jahnavim2105@gmail.com>
2024-08-01 19:14:50 -0400

    skip function update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Player from './components/Player'
 
 function App() {
-  const [songs, setSongs] = useState([
+  const [songs] = useState([
     {
       title: "I Was Made For Loving You",
       artist: "KISS",
@@ -20,11 +20,25 @@ function App() {
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
 
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if(currentSongIndex + 1 > songs.length-1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
+
   return (
     <div className="App">
       <h1>HarmonyBox</h1>
-      <Player song={songs[currentSongIndex]} 
-      nextSong={songs[nextSongIndex]}/>
+      <Player 
+      currentSongIndex={currentSongIndex}
+      setCurrentSongIndex={setCurrentSongIndex}
+      nextSongIndex={nextSongIndex}
+      songs={songs}
+      />
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,16 +1,55 @@
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import PlayerDetails from './PlayerDetails'
 import PlayerControls from './PlayerControls'
 
 
 function Player(props) {
+    const audioElm = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    useEffect(() => {
+        if(isPlaying){
+            audioElm.current.play();
+        } else {
+            audioElm.current.pause();
+        }
+    });
+
+    const SkipSong = (forwards = true) => {
+        if(forwards){
+            props.setCurrentSongIndex(() => {
+                let temp = props.currentSongIndex;
+                temp++;
+
+                if(temp > props.songs.length -1){
+                    temp = 0;
+                }
+                return temp;
+            });
+        } else {
+            props.setCurrentSongIndex(() => {
+                let temp = props.currentSongIndex;
+                temp--;
+
+                if(temp < 0){
+                    temp = props.songs.length - 1;
+                }
+                return temp;
+            });
+        }
+    }
+
   return (
     <div className='comp-player'>
-      <audio></audio>
+      <audio src={props.songs[props.currentSongIndex].src} ref={audioElm}></audio>
       <h4>Playing now</h4>
-        <PlayerDetails song={props.song} />
-        <PlayerControls />
-      <p><strong>Next up: </strong>{props.nextSong.title} by {props.nextSong.artist}</p>
+        <PlayerDetails song={props.songs[props.currentSongIndex]} />
+        <PlayerControls 
+            isPlaying={isPlaying} 
+            setIsPlaying={setIsPlaying} 
+            SkipSong={SkipSong}
+        />
+      <p><strong>Next up: </strong>{props.songs[props.nextSongIndex].title} by {props.songs[props.nextSongIndex].artist}</p>
     </div>
   )
 }
