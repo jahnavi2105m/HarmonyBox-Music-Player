da6f07311a95823adaa51ee9fc3a237586b571be
Jahnavi Malhotra <jahnavim2105@gmail.com>
2024-08-01 14:51:28 -0400

    buttons update

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PlayerDetails from './PlayerDetails'
+import PlayerControls from './PlayerControls'
+
 
 function Player(props) {
   return (
@@ -7,6 +9,7 @@ function Player(props) {
       <audio></audio>
       <h4>Playing now</h4>
         <PlayerDetails song={props.song} />
+        <PlayerControls />
       <p><strong>Next up: </strong>{props.nextSong.title} by {props.nextSong.artist}</p>
     </div>
   )
